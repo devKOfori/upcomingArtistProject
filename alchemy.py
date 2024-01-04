64b395fa4f272ba431a7704d6f62c1b1c7f54520
inconsistentDev <groupfivethree@gmail.com>
2024-01-04 13:42:52 +0200

    Read data from SQL table

--- alchemy.py
+++ alchemy.py
@@ -3,10 +3,15 @@ from sqlalchemy import text
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
-with engine.connect() as conn:
+with engine.begin() as conn:
     conn.execute(text("CREATE TABLE coordinates (x int, y int)"))
     conn.execute(
         text("INSERT INTO coordinates (x, y) VALUES (:x, :y)"), 
         [{"x": 1, "y": 2}, {"x": 0, "y": 1}]
     )
-    conn.commit()
+
+with engine.begin() as conn:
+    results = conn.execute(text("select * from coordinates"))
+    for row in results: 
+        print("x = ", row.x, "y = ", row.y)
+        
