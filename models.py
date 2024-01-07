8aee091c28466946b36515b2bcd4f061ed67112d
inconsistentDev <groupfivethree@gmail.com>
2024-01-07 15:58:18 +0200

    Run Initialize DB

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from datetime import date
 from typing import List
+from sqlalchemy import create_engine
 from sqlalchemy import Table
 from sqlalchemy import Column
 from sqlalchemy import String
@@ -7,7 +8,11 @@ from sqlalchemy import Text
 from sqlalchemy import Date
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+import settings
 
 
 class Base(DeclarativeBase):
@@ -16,16 +21,17 @@ class Base(DeclarativeBase):
 class SocialMedia(Base):
     __tablename__ = "socialmedia"
 
-    id: Mapped(int) = mapped_column(primary_key=True)
-    name: Mapped(str)
-    url: Mapped(str) 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    url: Mapped[str]
     artist: Mapped["Artist"] = relationship(back_populates="socialmedia")
 
 class Gender(Base):
-    __tablename__ = "Gender"
+    __tablename__ = "gender"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped(str)
+    id_gender: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    artists: Mapped[List["Artist"]] = relationship(back_populates="gender")
 
 class Artist(Base):
     __tablename__ = "artist"
@@ -33,7 +39,7 @@ class Artist(Base):
     id_artist: Mapped[int] = mapped_column(primary_key=True)
     full_name: Mapped[str]
     stage_name: Mapped[str]
-    gender: Mapped[Gender]
+    gender: Mapped[Gender] = relationship(back_populates="artists")
     email_address: Mapped[str | None]
     phone: Mapped[str | None]
     biography: Mapped[str] = mapped_column(Text, deferred=True)
@@ -52,7 +58,7 @@ class Genre(Base):
     __tablename__ = "genre"
 
     id_genre: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped(str)
+    name: Mapped[str]
 
 class SongType(Base):
     __tablename__ = "songtype"
@@ -84,3 +90,10 @@ collaboration_artists = Table(
     Column("id_collaboration", ForeignKey("collaboration.id_collaboration"), primary_key=True, index=True),
     Column("id_artist", ForeignKey("artist.id_artist"), primary_key=True, index=True)
 )
+try:
+    print("Initializing Database...")
+    engine = create_engine(settings.DATABASE_URL, echo="debug")
+    Base.metadata.create_all(bind=engine)
+    print("Database Initializing Successful...")
+except:
+    print("Error occured")
