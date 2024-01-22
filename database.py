6de4848fda245fea5e472b84ade17c59964b90f9
inconsistentDev <groupfivethree@gmail.com>
2024-01-22 02:08:46 +0200

    Connecting to PostgreSQL database

--- database.py
+++ database.py
@@ -2,19 +2,21 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-import settings
+from data import settings
 
 load_dotenv(".env")
 
 class Base(DeclarativeBase):
     pass
 
-def create_db_engine():
-    try:
-        print("Initializing Database...")
-        engine = create_engine(settings.DATABASE_URL, echo="debug")
-        Base.metadata.create_all(bind=engine)
-        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-        print("Initializing Database Completed...")
-    except:
-        print("Error occured")
+SessionLocal = ""
+try:
+    print("Initializing Database...")
+    engine = create_engine(settings.DATABASE_URL, echo="debug")
+    engine.connect()
+    print("Connected to database")
+    # Base.metadata.create_all(bind=engine)
+    # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    # print("Initializing Database Completed...")
+except:
+    print("An error occured while connecting to the database")
